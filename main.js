e8e971e9089479d23d3791e088619a33ab0c8f50
nikityane <eckose@gmail.com>
2018-10-25 19:14:59 +0300

    interface

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChart); 
 
 var mas = [['g','Хищник','Жертва']];
 var predatorCount = 0;
 var victimCount = 0;
-var rV = 100/1000;  //рождаемость жертвы
-var dV = 300/100000; //Смертность жертвы
-var dP = 70/1000; // смертность хищника
-var kP = 80/1000000; //коэф.хищника
+var vF = 100/1000;  //рождаемость жертвы
+var vM = 300/100000; //Смертность жертвы
+var pM = 70/1000; // смертность хищника
+var p = 80/1000000; //коэф.хищника
 var g = 500; //поколение
 var victimStart = 1000; // нач. кол-во жертв
 var predatorStart = 100; // нач. кол-во хищников
@@ -17,8 +17,8 @@ mas.push([0,predatorStart,victimStart]);
 //mas.push([0,predatorStart,victimStart]);
 		for(let i = 1; i <= g; i++){
 
-			predatorCount = Math.round(predatorStart + kP * predatorStart * victimStart - dP * predatorStart);
-        	victimCount = Math.round(victimStart + rV * victimStart - dV * victimStart * predatorStart);
+			predatorCount = Math.round(predatorStart + p * predatorStart * victimStart - pM * predatorStart);
+        	victimCount = Math.round(victimStart + vF * victimStart - vM * victimStart * predatorStart);
         	predatorStart = predatorCount;
         	victimStart = victimCount;
         	mas.push([i,predatorStart,victimStart]);
@@ -28,6 +28,20 @@ mas.push([0,predatorStart,victimStart]);
 		
 	}
 	console.log(mas);
+
+	function inputValue(){
+		vF = document.getElementById('victimFertility').value; 
+		vM = document.getElementById('victimFertility').value; 
+		pM = document.getElementById('victimFertility').value; 
+		p = document.getElementById('victimFertility').value; 
+		g = document.getElementById('victimFertility').value; 
+		victimStart = document.getElementById('victimFertility').value; 
+		predatorStart = document.getElementById('victimFertility').value; 
+	}
+
+	document.getElementById('start').onclick = function(){
+		console.log(document.getElementById('victimFertility').value);
+	} 
 	calcCount();
 
       function drawChart() {
@@ -36,7 +50,10 @@ mas.push([0,predatorStart,victimStart]);
         var options = {
           title: 'Модель Хищник-Жертва',
           curveType: 'function',
-          legend: { position: 'bottom' }
+          legend: { position: 'bottom' },
+          crosshair: { trigger: 'both' },
+          explorer: { axis: 'horizontal' },
+          textStyle: {color: '#FF0000'}, showColorCode: true
         };
 
         var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
